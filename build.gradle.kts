2c78c108aa4ba45173f041cad0139ad566a02f78
Nick Sanft <nsanft@sanft.com>
2026-02-22 15:25:52 -0600

    Added ktor libraries, jpos

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,21 @@ kotlin {
     jvmToolchain(21)
 }
 
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-html-builder")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("org.jpos:jpos:3.0.1")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
